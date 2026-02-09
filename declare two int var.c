af6532aff230173766b4646de85debe8ac9c367c
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-09 13:29:41 +0530

    Add files via upload

---
+++ declare two int var.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    int c,d;
+    scanf("%d %d",&a,&b);
+    scanf("%d %d",&c,&d);
+    printf("%d %d\n",a,b);
+    printf("%d %d",c,d);
+    
+    
+    return 0;
+}
