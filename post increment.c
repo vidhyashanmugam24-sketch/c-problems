ca9a3188748fbf1b4158271ee12c2a60b8966890
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-09 16:36:42 +0530

    Add files via upload

---
+++ post increment.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+  int a;
+  int b;
+  int c;
+  scanf("%d",&a);
+  printf("%d",a++);
+  scanf("%d",&b);
+  printf("%d",b++);
+  scanf("%d",&c);
+  printf("%d",c++);
+    return 0;
+}
