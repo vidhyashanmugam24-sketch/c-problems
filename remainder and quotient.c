b85a5a45a4d9baa7954b813d68b0317b8c8180c0
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-09 16:22:43 +0530

    Add files via upload

---
+++ remainder and quotient.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int a,b;
+  int c,d;
+  int e,f;
+  scanf("%d %d",&a,&b);
+   printf("Quotient=%d  Remainder=%d",a/b,a%b);
+   scanf("%d %d",&c,&d);
+   printf("Quotient=%d  Remainder=%d",c/d,c%d);
+   scanf("%d %d",&e,&f);
+   printf("Quotient=%d  Remainder=%d",e/f,e%f);
+  
+    return 0;
+}
