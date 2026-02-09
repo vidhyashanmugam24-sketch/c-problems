248aba12651eca7107d4e399c8dac9e2b8ee9fa5
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-09 14:23:08 +0530

    Add files via upload

---
+++ mul two var.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+     int a=10;
+     int b=2;
+     int c=a*b;
+     int n1=5;
+     int n2=10;
+     int n3=n1*n2;
+     printf("%d\n%d",c,n3);
+     
+
+    return 0;
+}
