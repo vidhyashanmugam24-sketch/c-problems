0a62b3a353b891446fed60a2e8211593ff4c5700
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-09 14:12:36 +0530

    Add files via upload

---
+++ add two var.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+     int a=5;
+     int b=10;
+     int c=a+b;
+     int n1=10;
+     int n2=20;
+     int n3=n1+n2;
+     printf("%d\n%d",c,n3);
+     
+
+    return 0;
+}
