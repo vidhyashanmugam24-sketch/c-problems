809c5a6cc6234acddb2e3c5f5897bf4e3272ffc6
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-09 14:17:23 +0530

    Add files via upload

---
+++ sub two var.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+     int a=20;
+     int b=15;
+     int c=a-b;
+     int n1=20;
+     int n2=10;
+     int n3=n1-n2;
+     printf("%d\n%d",c,n3);
+     
+
+    return 0;
+}
