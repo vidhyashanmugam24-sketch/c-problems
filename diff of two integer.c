0ccbf25e6c01f86cf8967f6f70aab3652da1cef2
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-09 16:09:01 +0530

    Add files via upload

---
+++ diff of two integer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int a,b;
+  int c,d;
+  int e,f;
+  scanf("%d %d",&a,&b);
+   printf("%d\n",a-b);
+  scanf("%d %d",&c,&d);
+  printf("%d",c-d);
+  scanf("%d %d",&e,&f);
+  printf("%d",e-f);
+    return 0;
+}
