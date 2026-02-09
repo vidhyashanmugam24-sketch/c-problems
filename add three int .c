e85658b400a57e736beca3d9717e7bf011d221c0
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-09 16:28:18 +0530

    Add files via upload

---
+++ add three int .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int a,b,c;
+  int d,e,f;
+  int g,h,i;
+  scanf("%d %d %d",&a,&b,&c);
+   printf("%d",a+b+c);
+   scanf("%d %d %d",&d,&e,&f);
+   printf("%d",d+e+f);
+   scanf("%d %d %d",&g,&h,&i);
+   printf("%d",g+h+i);
+  
+    return 0;
+}
