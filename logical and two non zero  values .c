8495936eac73de7aaf91dfda352cd986ac2c7112
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-10 11:33:25 +0530

    Add files via upload

---
+++ logical and two non zero  values .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=5,b=3;
+    int c=1,d=1;
+    int e=0,f=5;
+    printf("%d\n",a && b);
+    printf("%d\n",c && d);
+    printf("%d",e && f);
+
+    return 0;
+}
