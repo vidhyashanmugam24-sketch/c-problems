9c2015d20b781b1aff6c0464fd1876444f740aab
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-10 10:23:16 +0530

    Add files via upload

---
+++ compare two integer using not equal to.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=5,b=3;
+    int c=4,d=4;
+    int e=9,f=1;
+    printf("%d\n",a!=b);
+    printf("%d\n",c!=d);
+    printf("%d\n",e!=f);
+
+
+
+    return 0;
+}
