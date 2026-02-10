7a737bf96d843fb8499c995f4a138563aa3d77cd
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-10 10:01:57 +0530

    Add files via upload

---
+++ increment twice.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    int y;
+    int z;
+    scanf("%d",&x);
+    x++;
+    x++;
+    printf("%d",x);
+    scanf("%d",&y);
+    y++;
+    y++;
+    printf("%d",y);
+    scanf("%d",&z);
+    z++;
+    z++;
+    printf("%d",z);
+
+    return 0;
+}
