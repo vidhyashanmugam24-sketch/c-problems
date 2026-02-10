fd7b112290a9b7fd0109497c00395c6331740687
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-10 12:03:50 +0530

    Add files via upload

---
+++ use +=operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10,b=5;
+    printf("%d\n",a+=b);
+    int c=3,d=7;
+    printf("%d\n",c+=d);
+    int e=0,f=4;
+    printf("%d",e+=f);
+    
+    return 0;
+}
