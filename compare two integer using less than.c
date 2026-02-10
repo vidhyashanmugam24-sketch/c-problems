4503d8ff80e59f61954021109e1ded8fb7017b2a
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-10 10:17:04 +0530

    Add files via upload

---
+++ compare two integer using less than.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    int c,d;
+    int e,f;
+    scanf("%d %d",&a,&b);
+    printf("%d",a>b);
+    scanf("%d %d",&c,&d);
+    printf("%d",c>d);
+    scanf("%d %d",&e,&f);
+    printf("%d",e>f);
+
+
+
+    return 0;
+}
