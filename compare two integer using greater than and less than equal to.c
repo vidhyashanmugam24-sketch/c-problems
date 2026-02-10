f4360e763ae4d03888fc79e0a9764340637f9279
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-10 10:29:01 +0530

    Add files via upload

---
+++ compare two integer using greater than and less than equal to.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=5,b=5;
+    int c=10,d=3;
+    int e=3,f=7;
+    printf("%d %d\n",a>=b,a>=b);
+    printf("%d %d\n",c>=d,a<=b);
+    printf("%d %d\n",e>=f,a<=b);
+
+
+
+    return 0;
+}
