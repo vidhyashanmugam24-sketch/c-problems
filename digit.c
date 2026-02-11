abd96137df4e2df6e35a565ca8a04f39907faca4
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-11 15:00:13 +0530

    Add files via upload

---
+++ digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch='7';
+    if(ch>='0'&& ch<='9'){
+        printf("digit");
+    }
+    else{
+        printf("not digit");
+    }
+
+    return 0;
+}
