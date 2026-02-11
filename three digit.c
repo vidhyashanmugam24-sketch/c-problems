894dfb3c7e99c95fa81e632973b043ef337f2c84
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-11 14:24:41 +0530

    Add files via upload

---
+++ three digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int num=345;
+  if(num>=100&&num<=999){
+      printf("three digit number");
+  }
+  else{
+      printf("not three digit number");
+  }
+
+    return 0;
+}
