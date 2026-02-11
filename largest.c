ccab90cfae7d6fac5276ac5bc542d0de896906fc
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-11 14:27:53 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int a=10;
+  int b=25;
+  int c=15;
+ if(a<b){
+     printf("25 is largest");
+ }
+ else if(b>c){
+     printf("10 is lesser");
+ }
+ else{
+     printf("number");
+ }
+ 
+
+    return 0;
+}
