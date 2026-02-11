bc9cedd9b13567325e2be800d435a43daef6d36b
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-11 14:53:02 +0530

    Add files via upload

---
+++ equilateral triangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int a=5,b=5,c=5;
+  if(a==b&&b==c){
+      printf("equilateral triangle");
+  }
+  else{
+      printf("not equilateral");
+  }
+ 
+
+    return 0;
+}
