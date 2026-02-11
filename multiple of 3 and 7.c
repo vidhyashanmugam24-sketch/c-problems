e339bfe39543515d891a05c2d3435b06b67ef28c
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-11 13:49:01 +0530

    Add files via upload

---
+++ multiple of 3 and 7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int num=21;
+   if(num%3==0&&num%7==0){
+       printf("multiple of both 3and 7");
+   }
+   else{
+       printf("multiple not 3 and 7");
+   }
+
+    return 0;
+}
