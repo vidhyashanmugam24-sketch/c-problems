d95c64a039e6150fc42d2e5e6dd074896952ae5e
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-11 14:41:17 +0530

    Add files via upload

---
+++ valid triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int sum,x=60,y=60,z=60;
+  sum=x+y+z;
+  if(sum==180){
+      printf("Valid triangle");
+  }
+  else{
+      printf("not valid");
+  }
+ 
+
+    return 0;
+}
