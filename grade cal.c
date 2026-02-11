d1ef998dadae1518fdff44650443a9ebed3a8ee8
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-11 14:31:03 +0530

    Add files via upload

---
+++ grade cal.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int n=82;
+  if(n>=90){
+      printf("Grade A");
+  }
+  if(n>=75){
+      printf("Grade B");
+  }
+  else{
+      printf("Grade C");
+  }
+ 
+
+    return 0;
+}
