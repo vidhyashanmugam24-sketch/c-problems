da86432f4976456d16abec5368df05a022f3a783
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-11 14:37:42 +0530

    Add files via upload

---
+++ divisible by 11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int num=121;
+  if(num%11==0){
+      printf("Divisible by 11");
+  }
+  else{
+      printf("not divisible by 11");
+  }
+  
+ 
+
+    return 0;
+}
