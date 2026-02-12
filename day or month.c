1e4ed741140dd7d60301e68e67fa738821ebcb38
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-12 10:27:59 +0530

    Add files via upload

---
+++ day or month.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int month=2;
+    switch(month){
+        case 1:
+        printf("31 days");
+        break;
+        case 2:
+        printf("28 or 29 days");
+        break;
+        case 3:
+        printf("31 days");
+        break;
+        case 4:
+        printf("30 days");
+        break;
+        case 5:
+        printf("31 days");
+        break;
+        case 6:
+        printf("30 days");
+        break;
+        case 7:
+        printf("31 days");
+        break;
+        case 8:
+        printf("31 days");
+        break;
+        case 9:
+        printf("30 days");
+        break;
+        case 10:
+        printf("31 days");
+        break;
+        case 11:
+        printf("30 days");
+        break;
+        case 12:
+        printf("31 days");
+        break;
+    }
+    return 0;
+}
