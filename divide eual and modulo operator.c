ca06551fe128922f909fc8034bffcf8f0b504eb6
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-12 11:55:54 +0530

    Add files via upload

---
+++ divide eual and modulo operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a=9;
+int b=2;
+printf("%d %d\n",a/b);
+int c=10;
+int d=5;
+printf("%d %d\n",c/d);
+int e=7;
+int f=3;
+printf("%d %d",e/f);
+    return 0;
+}
