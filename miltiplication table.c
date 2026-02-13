e867c228647c3eddf7a1f3ef44a6a927c52fdea9
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-13 10:29:28 +0530

    Add files via upload

---
+++ miltiplication table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=10;i++){
+        printf("%d ",i*5);
+    }
+    
+    
+        
+    
+    
+    return 0;
+}
