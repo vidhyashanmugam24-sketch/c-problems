d641a26a26e48707ce1c7d019beeaee088275441
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-17 12:22:38 +0530

    Add files via upload

---
+++ Prime Numbers Counter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    
+       int n,i,count=0;
+       scanf("%d",&n);
+       for(i=0;i<=n;i++){
+          if(i==1||i==3||i==5||i==7){
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
