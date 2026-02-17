c6b451b5ca4a88f7da9ea78ee6136fa300b6d3de
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-02-17 11:29:18 +0530

    Add files via upload

---
+++ for loop 16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int i,n,sum=0;
+    scanf("%d",&n);
+    for(i=1;i<=n/2;i++){
+        if(n%i==0)
+        sum=sum+i;
+    }
+    if(sum==n){
+        printf("yes",n);
+    }
+    else
+    {
+        printf("no",n);
+    }
+}
