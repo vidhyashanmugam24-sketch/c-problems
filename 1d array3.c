e9299524ce58a26340be274fc12d05719446030b
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-03-03 09:34:12 +0530

    Add files via upload

---
+++ 1d array3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int sales[n];
+    for(int i=0;i<n;i++) {
+        scanf("%d",&sales[i]);
+    }
+    int sorted=1;   
+    for(int i=0;i<n-1;i++) {
+        if(sales[i]>sales[i+1]) {
+            sorted=0;   
+            break;
+        }
+    }
+    if(sorted==1) {
+        printf("Yes");
+    } else {
+        printf("No");
+    }
+    return 0;
+}
