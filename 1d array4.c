1e65cb23accf3f2c439613f105fbcdafc5c49b02
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-03-03 09:39:38 +0530

    Add files via upload

---
+++ 1d array4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    int i=0,j=n-1,temp;
+    while(i<j) {
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+        i++;
+        j--;
+    }
+    for(int i=0;i<n;i++)
+        printf("%d ",a[i]);
+    return 0;
+}
