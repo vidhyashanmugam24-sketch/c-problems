b060e5d4df3104e58f1fc1901eddd262dec21d3e
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-03-11 10:10:16 +0530

    Add files via upload

---
+++ str 3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    char email[100];
+    int i=0;
+    scanf("%s",email);
+    while(email[i] != '@') {
+        printf("%c",email[i]);
+        i++;
+    }
+    return 0;
+}
